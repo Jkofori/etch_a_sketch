11227bb20c13ed1f497fee95aa7e9a3bf7a434e7
Justin Ofori <justinkofori@gmail.com>
2021-05-15 09:06:06 -0700

    Create reset button, allowing user input between 1-100 to reset grid with specified grid size. Add what I learned to readme.md

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-In this project I will create a simple etch-a-sketch board that changes the colors of squares in a grid as the user's mouse hovers over it.
+In this project I will create a simple etch-a-sketch board that changes the colors of squares in a grid as the user's mouse hovers over it. A reset button will allow the user to clear the board and choose a grid that ranges between 1x1 and 100x100.
+
+What I learned:
+- How to dynamically place multiple divs within the UI
+- When to use event.target to manipulate elements
--- script.js
+++ script.js
@@ -1,26 +1,24 @@
-let gridSize = 25;
+function createGrid(gridSize = 16){
+    let container = document.getElementById('container');
 
+    // set number of columns equal to gridSize to create a 1:1 container
+    let square = '1fr ';
+    container.style.gridTemplateColumns = square.repeat(gridSize);
 
-let container = document.getElementById('container');
+    // create designated squares within container
+    for(let counter=0;counter<gridSize*gridSize;counter++){
+        let newDiv = document.createElement('div');
+        newDiv.className='box';
+        container.appendChild(newDiv);
+    }
 
-// set number of columns equal to gridSize to create a square
-let square = '1fr ';
-container.style.gridTemplateColumns = square.repeat(gridSize);
-
-// create designated grid within container
-for(let counter=0;counter<gridSize*gridSize;counter++){
-    let newDiv = document.createElement('div');
-    newDiv.className='box';
-    container.appendChild(newDiv);
-}
-
-let boxColor = document.getElementsByClassName('box');
-
-// add eventListener to change color of each square on hover
-for(count=0;count<boxColor.length;count++){
-    boxColor.item(count).addEventListener('mouseover',function(e){
+    // add eventListener to change color of each square on hover
+    let boxColor = document.getElementsByClassName('box');
+    for(count=0;count<boxColor.length;count++){
+    boxColor.item(count).addEventListener('mouseover',function(){
         event.target.style.backgroundColor = randomColor();
     });
+    }
 }
 
 // produce a random color for the etch-a-sketch
@@ -30,6 +28,40 @@ function randomColor(){
     return 'rgba(' + round(rand()*255) + ',' + round(rand()*255) + ',' 
     + round(rand()*255) + ',' + 1 + ')';
 }
+
+// reset grid when resetButton is clicked, take user input for new grid size
+document.getElementById('resetButton').addEventListener('click',function(){
+    let gridSize = updateGrid();
+    
+
+
+    // clear container, add new squares based on new grid size
+    document.getElementById('container').innerHTML = '';
+    createGrid(gridSize);
+});
+
+// take user input between 1 & 100 and return value
+function updateGrid(){
+    let newGridSize = prompt('Please enter a grid size:');
+
+    // if input invalid, repeat prompt until input is valid
+    if(newGridSize>100 || newGridSize<1 || isNaN(newGridSize)){
+        alert('Please enter a grid size between 1-100.')
+        newGridSize = updateGrid();
+    } else{
+        // return valid input after first try
+        return newGridSize;
+    }
+
+    // return valid input after 1+ invalid inputs
+    return newGridSize;
+}
+
+// create 16x16 grid on first load
+createGrid();
+
+
+
 
 
 
